7c48dbe9c282f341c6c6267db2e66b6b42501d29
rickzeira <135248321+rickzeira@users.noreply.github.com>
2024-04-22 12:17:36 -0300

    tempo

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo1 = new Date("2022-02-25T00:00:00")
 const tempoObjetivo2 = new Date("2024-10-05T00:00:00")
 const tempoObjetivo3 = new Date("2024-10-05T00:00:00")
 const tempoObjetivo4 = new Date("2024-10-05T00:00:00")
@@ -38,22 +38,22 @@ function atualizaCronometro(){
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000); }
-    comecaCronometro();
-
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
-
-function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24)
-
-    segundos %=60;
-    minutos %=60;
-    horas %=24;
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-}
+    
+    
+    
+    function calculaTempo(tempoObjetivo){
+        let tempoAtual = new Date();
+        let tempoFinal = tempoObjetivo - tempoAtual;
+        let segundos = Math.floor(tempoFinal/1000);
+        let minutos = Math.floor(segundos/60);
+        let horas = Math.floor(minutos/60);
+        let dias = Math.floor(horas/24)
+        
+        segundos %=60;
+        minutos %=60;
+        horas %=24;
+        
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }
+        comecaCronometro();
+        
